7175e1dbad57e0636e7173e0100a056b1a330e5a
Kelvin Yuli Andrian <121687899+kelvinzer0@users.noreply.github.com>
2023-08-29 23:41:44 +0700

    Update CryptonightR_template.h

--- src/cryptonight/CryptonightR_template.h
+++ src/cryptonight/CryptonightR_template.h
@@ -521,7 +521,7 @@ void CryptonightR_instruction_mov256(void);
 // extern const void* instructions[257];
 // extern const void* instructions_mov[257];
 
-inline const void* instructions[257] = {
+const void* instructions[257] = {
 	CryptonightR_instruction0,
 	CryptonightR_instruction1,
 	CryptonightR_instruction2,
@@ -781,7 +781,7 @@ inline const void* instructions[257] = {
 	CryptonightR_instruction256,
 };
 
-inline const void* instructions_mov[257] = {
+const void* instructions_mov[257] = {
 	CryptonightR_instruction_mov0,
 	CryptonightR_instruction_mov1,
 	CryptonightR_instruction_mov2,
